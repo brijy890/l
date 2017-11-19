2254da22fbc3d923be8efc3136582f665bbcf65f
brij <brijy123@gmail.com>
2017-11-19 17:09:30 +0530

    intial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Posts</h1>
+    @if(count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="well">
+                <h1><a href="/posts/{{$post->id}}">{{$post->title}}</a></h1>
+                <small>Written on{{$post->created_at}}</small>
+            </div>
+        @endforeach
+        {{$posts->links()}}
+    @else
+        <p>No Posts found</p>
+    @endif
+@endsection
