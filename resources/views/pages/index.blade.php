a77f026795ad79312a84d966b38c1ea3b87168dc
brij <brijy123@gmail.com>
2017-11-22 10:43:25 +0530

    style.css changed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
   <div class="h-image" style="background: url('{{ asset('images/image1.jpeg') }}') no-repeat center center;">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12 col-xs-12">
           <div class="jumbotron text-center">
             <h1>{{$title}}</h1>
             <p>Create your post and publish in blog!</p>
